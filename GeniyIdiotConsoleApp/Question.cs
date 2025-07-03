1116641eb2c82592666c4f3cf62a1328953d2a24
PolAleks <aa.polushkin@yandex.ru>
2025-07-03 15:31:29 +0300

    Создал класс Question

---
+++ GeniyIdiotConsoleApp/Question.cs
@@ -0,0 +1,13 @@
+﻿namespace GeniyIdiotConsoleApp
+{
+    public class Question
+    {
+        public string text;
+        public int answer;
+        public Question(string text, int answer)
+        {
+            this.text = text;
+            this.answer = answer;
+        }
+    }
+}
