ac7bb8c398232ec56b9dd9b4e6d82882cc05728c
PolAleks <aa.polushkin@yandex.ru>
2025-07-03 21:23:18 +0300

    Создал класс для работы с файловой системой FileServices

---
+++ GeniyIdiotConsoleApp/FileServices.cs
@@ -0,0 +1,34 @@
+﻿namespace GeniyIdiotConsoleApp
+{
+    public class FileServices
+    {
+        public static List<string> Load(string file)
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, file);
+            var lines = new List<string>();
+
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        lines.Add(sr.ReadLine());
+                    }
+                }
+            }
+
+            return lines;
+        }
+
+        public static void Save(string file, string content)
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, file);
+
+            using (StreamWriter sr = new StreamWriter(path, true, System.Text.Encoding.Default))
+            {
+                sr.WriteLine(content);
+            }
+        }
+    }
+}
