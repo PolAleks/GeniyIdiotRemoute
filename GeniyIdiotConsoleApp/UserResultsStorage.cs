3a2b71d86ce68bea3511a4d95365ac43aff9df92
PolAleks <aa.polushkin@yandex.ru>
2025-07-04 11:12:00 +0300

    добавил статическое поле _file для хранения имени файла с результатами тестирования

--- GeniyIdiotConsoleApp/UserResultsStorage.cs
+++ GeniyIdiotConsoleApp/UserResultsStorage.cs
@@ -2,27 +2,26 @@
 {
     public class UserResultsStorage
     {
+        readonly static string _file = "log.txt";
         public static void Save(User user)
         {
-            string file = "log.txt";
             string content = $"{user.name}#{user.countCorrectAnswer}#{user.diagnosis}";
 
-            FileServices.Save(file, content);
+            FileServices.Save(_file, content);
         }
 
         public static List<User> GetAll()
         {
             var users = new List<User>();
-            var file = "log.txt";
-
-            foreach(var line in FileServices.Load(file)) 
+            
+            foreach (var line in FileServices.Load(_file))
             {
                 var item = line.Split('#');
                 (string name, int countCorrectAnswer, string diagnosis) = (item[0], Convert.ToInt32(item[1]), item[2]);
                 users.Add(new User(name, countCorrectAnswer, diagnosis));
             }
-        
+
             return users;
         }
-}
+    }
 }
