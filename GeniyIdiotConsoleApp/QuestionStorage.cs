1e1f7b7b909bfb4b4695f23b1586241b5be0fd44
PolAleks <aa.polushkin@yandex.ru>
2025-07-07 11:41:26 +0300

    В QuestionStorage.GetAll() добавил первичную инициализацию вопросов, если отсутсвует файл с вопросами

--- GeniyIdiotConsoleApp/QuestionStorage.cs
+++ GeniyIdiotConsoleApp/QuestionStorage.cs
@@ -6,14 +6,32 @@
 
         public static List<Question> GetAll()
         {
-            var lines = FileServices.Load(_file);
-
             var questions = new List<Question>();
-            foreach (var line in lines)
+
+            if (FileServices.Exists(_file))
             {
-                var item = line.Split('#');
-                (string textQuestion, int answerQuestion) = (item[0], Convert.ToInt32(item[1]));
-                questions.Add(new Question(textQuestion, answerQuestion));
+
+                var lines = FileServices.Load(_file);
+
+                foreach (var line in lines)
+                {
+                    var item = line.Split('#');
+                    (string textQuestion, int answerQuestion) = (item[0], Convert.ToInt32(item[1]));
+                    questions.Add(new Question(textQuestion, answerQuestion));
+                }
+            }
+            else
+            {
+                questions = [new Question("Сколько будет два плюс два умноженное на два?", 6),
+                             new Question("Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?", 9),
+                             new Question("На двух руках 10 пальцев. Сколько пальцев на 5 руках?", 25),
+                             new Question("Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?", 60),
+                             new Question("Пять свечей горело, две потухли. Сколько свечей осталось?", 2),];
+
+                foreach (var question in questions)
+                {
+                    Add(question);
+                }
             }
 
             Shuffle(questions);
